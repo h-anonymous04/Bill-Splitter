a4fe158ba6df285cf383011bb6989a8d5e6015b7
Het Shah <shahhet6153@gmail.com>
2023-10-18 20:48:47 +0530

    Added animation

--- src/app/components/expenses/expense/expense.component.ts
+++ src/app/components/expenses/expense/expense.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Expense } from '../../expense.model';
-import { Person } from '../../person.model';
 
 @Component({
   selector: 'app-expense',
